a5f12f02e20e2817e2bd3ccd2c8a96e0e453e1c6
rahul <rs009161@gmail.com>
2024-12-23 15:14:24 +0530

    change id

--- src/app/cloudinary/page.tsx
+++ src/app/cloudinary/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+
 import { CldUploadButton } from 'next-cloudinary';
+
 export default function Page() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
